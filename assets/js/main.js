8dfa379b63ce728566cf8791213fa1bf46242333
bethanyrbs <bethsmallwood09@gmail.com>
2023-04-14 21:16:53 +0100

    hi

---
+++ assets/js/main.js
@@ -0,0 +1,135 @@
+jQuery(window).on('load', function() {
+	"use strict";
+    
+    // When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+} 
+
+    // HIDE PRELOADER
+    $(".preloader").addClass("hide-preloader");   
+    
+    // SHOW/ANIMATE ANIMATION CONTAINER
+    setTimeout(function(){
+
+        $("#intro .animation-container").each(function() {
+
+            var e = $(this);
+
+            setTimeout(function(){
+
+                e.addClass("run-animation");
+
+            }, e.data("animation-delay") );
+
+        });
+
+    }, 700 );
+
+    
+});
+
+
+jQuery(document).ready(function($) {
+	"use strict";
+    
+    
+    // SMOOTH SCROLL FOR SAME PAGE LINKS
+    $(document).on('click', 'a.smooth-scroll', function(event) {
+        
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top - 80
+        }, 500);
+        
+    });
+    
+    
+    // SCROLL REVEAL SETUP
+    window.sr = ScrollReveal();
+    sr.reveal(".scroll-animated", { 
+        duration: 600,
+        delay: 0,
+        origin: "left",
+        rotate: { x: 0, y: 0, z: 0 },
+        opacity: 0,
+        distance: "20vh",
+        viewFactor: 0.4,
+        scale: 1,
+    });
+    
+    
+    // AJAX CONTACT FORM SUBMIT
+    $("#contact-form").submit(function(e) {
+
+        e.preventDefault();
+        var postdata = $(this).serialize();
+
+        $.ajax({
+
+            type: "POST",
+            url: "assets/php/contact.php",
+            data: postdata,
+            dataType: "json",
+            success: function(json) {
+
+                $("#contact-form input, #contact-form textarea").removeClass("error");
+
+                setTimeout(function(){
+
+                    if (json.nameMessage !== "") {
+
+                        $("#contact-form-name").addClass("error");
+
+                    }
+
+                    if (json.emailMessage !== "") {
+
+                        $("#contact-form-email").addClass("error");
+
+                    }
+
+                    if (json.messageMessage !== "") {
+
+                        $("#contact-form-message").addClass("error");
+
+                    }
+
+                }, 10);
+
+                if (json.nameMessage === "" && json.emailMessage === "" && json.messageMessage === "") {
+
+                    $("#contact-form.error input, #contact-form.error textarea").removeClass("error");
+                    $('#contact-form').addClass("success");
+                    $('#contact-form textarea, #contact-form input').val("");
+                    
+                    setTimeout(function(){
+                        
+                        $('#contact-form').removeClass("success");
+                        
+                    },4000);
+
+                }
+
+            }
+
+        });
+
+    });
+
+    
+});
